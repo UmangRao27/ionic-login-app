351c1d2dcbc2eeeb9230e5f038f11daf56071638
Bhargavp001 <125031611+Bhargavp001@users.noreply.github.com>
2026-02-06 10:22:37 +0530

    some changes

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Platform,AlertController } from '@ionic/angular';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-login',
@@ -10,24 +12,39 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class LoginPage implements OnInit {
 
+  backButtonSub : any;
+  showPassword : boolean = false ; 
+
   loginForm! : FormGroup ;
 
-  constructor(private fb: FormBuilder, private router : Router) {
+  constructor(private fb: FormBuilder, private router : Router, private platform : Platform, private alertctrl : AlertController) {
 
   }
 
   ngOnInit() {
+    const savedUsername = localStorage.getItem('savedUsername') || '';
+    const savedPassword = localStorage.getItem('savedPassword') || '';
     this.loginForm = this.fb.group({
-      username : ['', Validators.required],
-      password : ['', Validators.required]
+      username : [savedUsername , Validators.required],
+      password : [savedPassword , Validators.required],
+      rememberMe : [savedUsername !== '']
     })
   }
 
   login() {
       if(this.loginForm.valid){
-        const { username, password } = this.loginForm.value;
+        const { username, password, rememberMe } = this.loginForm.value;
 
+        if(rememberMe){
+          localStorage.setItem('savedUsername', username);
+          localStorage.setItem('savedPassword', password);
+        }
+        else{
+          localStorage.removeItem('savedUsername');
+          localStorage.removeItem('savedPassword');
+        }
         sessionStorage.setItem('fromLogin','true');
+
 
         this.router.navigate(['/home'],{
           queryParams : { username, password }
@@ -41,6 +58,32 @@ export class LoginPage implements OnInit {
   isInvalid(field : string) {
     const control = this.loginForm.get(field);
     return control?.invalid && ( control?.touched || control?.dirty )
+  }
+
+  ionViewWillEnter() {
+    this.backButtonSub = this.platform.backButton.subscribeWithPriority(10, async () => {
+      const alert = await this.alertctrl.create({
+        header : 'Exit App',
+        message : 'Do You Want to exit App ?',
+        buttons : [
+          {
+            text : 'Cancel',
+            role : 'cancel',
+          },
+          {
+            text : 'Exit',
+            handler: () => {
+              App.exitApp();
+            }
+          }
+        ]
+      });
+      await alert.present();
+    })
+  }
+
+  ionViewWillLeave(){
+    this.backButtonSub.unsubscribe();
   }
 
 }
